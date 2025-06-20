42a5b08bc4e185cc67a94eb29d66e948596e60fb
PatriciaSkowronek <skowronek@biochem.mpg.de>
2025-06-20 13:16:44 +0200

    Adjusted current agents to interact with database agent correctly

--- proteomics_specialist/agent.py
+++ proteomics_specialist/agent.py
@@ -3,6 +3,7 @@ from google.adk.tools.agent_tool import AgentTool
 
 from . import prompt
 from .sub_agents.alphakraken import alphakraken_agent
+from .sub_agents.database import database_agent
 
 # MODEL = "gemini-2.5-pro-preview-03-25"
 MODEL = "gemini-2.5-flash"
@@ -10,10 +11,10 @@ MODEL = "gemini-2.5-flash"
 adviser_coordinator = LlmAgent(
    name="ai_proteomics_adviser",
    model=MODEL,
-   description="""Agent to support proteomics researchers by providing the user with instrument
-                performance status.""",
+   description="""Agent to support proteomics researchers.
+   """,
    instruction=prompt.PROMPT,
-   tools=[AgentTool(agent=alphakraken_agent)]
+   tools=[AgentTool(agent=alphakraken_agent), AgentTool(agent=database_agent)]
 )
 
 root_agent = adviser_coordinator
--- proteomics_specialist/prompt.py
+++ proteomics_specialist/prompt.py
@@ -6,4 +6,9 @@ You are a highly proactive assistant and advisor with a broad knowledge of prote
 - Minimize Clarification: Only ask clarifying questions if the user's intent is highly ambiguous and reasonable defaults cannot be inferred. Strive to act on the request using your best judgment.
 - Provide concise, direct answers based on tool output. Format information for easy readability.
 - If some information cannot be determined, ask for clarification.
+
+# Strategies:
+- You provide the user with instrument status using the 'alphakraken_agent' and close your answer with "Would you measure with this performance or do you need helo with the decision?"
+- You write the sentiment of a user regarding a performance in the performance_data table with 'database_agent'
+- You help if someone needs assistance in interpeting performance results by first using the 'database_agent' to find entries of the same same instrument (= instrument_id) and gradient (=Raw Gradient Length (m)). Then you use the alphakraken to get the exect details for these file_names.
 """
