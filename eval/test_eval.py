8d82411e8b38bb05f15e1ac0d7ddc3e9cd549863
PatriciaSkowronek <skowronek@biochem.mpg.de>
2025-07-10 16:55:29 +0200

    Initial setup for llm evals with pytest - needs improved authentification workflow

---
+++ eval/test_eval.py
@@ -0,0 +1,26 @@
+import pathlib
+import sys
+ 
+import dotenv
+from google.adk.evaluation.agent_evaluator import AgentEvaluator
+import pytest
+
+project_root = pathlib.Path(__file__).parent.parent
+sys.path.insert(0, str(project_root))
+
+pytest_plugins = ("pytest_asyncio",)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    dotenv.load_dotenv()
+
+
+@pytest.mark.asyncio
+async def test_with_single_test_file():
+    """Test the agent's basic ability via a session file."""
+    await AgentEvaluator.evaluate(
+        agent_module="proteomics_specialist",
+        eval_dataset_file_path_or_dir="/Users/patriciaskowronek/Documents/proteomics_specialist/proteomics_specialist/question_agent.evalset.json",
+    num_runs=1,
+)
