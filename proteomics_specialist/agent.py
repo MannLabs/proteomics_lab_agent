3b5625deb35f2f1d6268b6142cbc54841b1be4e3
PatriciaSkowronek <skowronek@biochem.mpg.de>
2025-07-10 18:27:45 +0200

    renamed sub agents

--- proteomics_specialist/agent.py
+++ proteomics_specialist/agent.py
@@ -2,19 +2,20 @@ from google.adk.agents import LlmAgent
 from google.adk.tools.agent_tool import AgentTool
 
 from . import prompt
-from .sub_agents.alphakraken import alphakraken_agent
-from .sub_agents.database import database_agent
+from .sub_agents.alphakraken_agent import alphakraken_agent
+from .sub_agents.database_agent import database_agent
+from .sub_agents.protocol_agent import protocol_agent
 
-# MODEL = "gemini-2.5-pro-preview-03-25"
 MODEL = "gemini-2.5-flash"
 
-adviser_coordinator = LlmAgent(
+root_agent = LlmAgent(
    name="ai_proteomics_adviser",
    model=MODEL,
-   description="""Agent to support proteomics researchers.
-   """,
+   description="""Agent to support proteomics researchers.""",
    instruction=prompt.PROMPT,
-   tools=[AgentTool(agent=alphakraken_agent), AgentTool(agent=database_agent)]
+   tools=[
+      AgentTool(agent=alphakraken_agent), 
+      AgentTool(agent=database_agent),
+      AgentTool(agent=protocol_agent)
+      ]
 )
-
-root_agent = adviser_coordinator
