6afddffec2d829e87df29453b96b7d931fe69cb2
PatriciaSkowronek <skowronek@biochem.mpg.de>
2025-08-21 13:24:15 +0200

    fix

--- proteomics_specialist/sub_agents/instrument_agent/prompt.py
+++ proteomics_specialist/sub_agents/instrument_agent/prompt.py
@@ -25,7 +25,7 @@ You are an expert in interacting with a database and you proactively answer user
  * B) If the correct instrument_id is used, proactively search for entries in the last 14 days with 'max_age_in_days' and present the user with the metrics. Repeat this pattern by adding 7 days to your query until you find quality metrics.
  * Inform the user which instrument_id and timeframe you used in the end to retrieve the results.
 
-- if you only get the proteins but not the precursors information then trigger the query again and search for the full information (Precursors, FWHM RT, Calibration MS1 Median Accuracy, Calibration MS2 Median Accuracy, Raw Gradient Length (m), Precursor Intensity Median)
+- if you only get the proteins but not the precursors information then trigger the query again and search for the full information (instrument_id, precursors, FWHM RT, Calibration MS1 Median Accuracy, Calibration MS2 Median Accuracy, Raw Gradient Length (m), Precursor Intensity Median)
 
 2) If someone asks for analysis results of a specific raw_file_name then use the 'get_raw_files_by_names' tool.
 You can invoke this function for one raw_file_name at once and then multiple times or for multiple raw_file_names once.
